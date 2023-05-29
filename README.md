cda383000ccb2d1bf108007fb3a6c7978f9efe90
mbugti04 <mustafammbg@gmail.com>
2023-05-29 03:39:55 -0500

    wave simulation

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # buffer-wave-simulation
+
+https://p5js.org/reference/
+
+https://web.archive.org/web/20160418004149/http://freespace.virgin.net/hugo.elias/graphics/x_water.htm
--- index.js
+++ index.js
@@ -1,28 +1,95 @@
-let buffer1; // 
-let buffer2; // 
+// two buffers (array of all pixels) are needed in order to calculate the wave movement
+let buffer1;
+let buffer2;
 
-const damping = 0.999 // non integer between 0 and 1
+// decay of wave
+// non-integer between 0 and 1
+const damping = .999;
 
 // size of array
-const ARRAY_WIDTH = 10;
-const ARRAY_HEIGHT = 10;
+const arrWidth = 400;
+const arrHeight = 400;
 
-// initialise arrays
-// TODO byte arrays? word arrays?
-buffer1 = Array(ARRAY_HEIGHT).fill().map(()=>Array(ARRAY_WIDTH).fill(0));
-buffer2 = Array(ARRAY_HEIGHT).fill().map(()=>Array(ARRAY_WIDTH).fill(0));
+// size of canvas
+//
 
-// main loop
-const processWater = () => {
+// emittor location
+const emittorX = 200;
+const emittorY = 25;
+// emitting interval (ms)
+const emittorInterval = 1000;
+
+// emitting length (ms)
+// leave -1 for limitless
+const emittingLength = 5000;
+
+// frame rate
+const fr = 30;
+
+// opacity of wave*?
+const waveValue = 5000;
+
+function setup() {
+    // Sets the pixel scaling for high pixel density displays
+    pixelDensity(1);
+    createCanvas(arrWidth, arrHeight);
+    // frame rate
+    frameRate(fr)
+
+    // initialise arrays
+    // TODO byte arrays? word arrays?
+    buffer1 = Array(arrWidth).fill(0).map(()=>Array(arrHeight).fill(0));
+    buffer2 = Array(arrWidth).fill(0).map(()=>Array(arrHeight).fill(0));
+}  
+
+function mouseClicked() {
+    // create source of wave at click of mouse
+    buffer1[mouseX][mouseY] = waveValue;
+}
+
+function draw() {
+    // wave emittor
+    if (frameCount < fr * emittingLength / 1000 || emittingLength == -1 ) {
+        if (frameCount % (fr * emittorInterval/1000) === 0) {
+            buffer1[emittorX][emittorY] = waveValue;
+        }
+    }
+
+
+    // needed for pixel modification
+    loadPixels();
+    // main loop
     for (let x = 1; x < buffer1.length - 1; x++) {
         for (let y = 1; y < buffer1.length - 1; y++) {
+            // explain*
             buffer2[x][y] = ( buffer1[x-1][y]
                             + buffer1[x+1][y] 
                             + buffer1[x][y+1]
                             + buffer1[x][y-1])
                             / 2
                             - buffer2[x][y];
+
+            // explain*
+            buffer2[x][y] = buffer2[x][y] * damping;
+            
+
+            let b2 = buffer2[x][y];
+            let index = (x + y * arrWidth) * 4;
+
+            // RGBA value of wave 
+            pixels[index + 0] = 0;
+            pixels[index + 1] = 0;
+            pixels[index + 2] = 255;
+            pixels[index + 3] = b2;
         }
-        buffer2[x][y] = buffer2[x][y] * damping;
+        
     }
+    updatePixels();
+    //*
+    let temp = buffer1;
+	buffer1 = buffer2;
+	buffer2 = temp;
+
+    let timePassed = Math.round((frameCount / fr) * 10) / 10;
+    text(timePassed, width / 2, height / 2);
 }
